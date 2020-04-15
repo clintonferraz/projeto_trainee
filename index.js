e8b08e26e2413d9e04ec00ac5f605e424bae6d60
Clinton <clinton_ferraz@hotmail.com>
2020-04-15 20:23:43 -0300

    header and drop menu

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 showMenu = () => {
-    
+    dropMenu = document.querySelector(".drop-menu");
+    dropMenu.classList.remove("hidden");
+};
+
+hideMenu = () => {
+    dropMenu = document.querySelector(".drop-menu");
+    dropMenu.classList.add("hidden");
 };
