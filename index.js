243b4615f84371bb29b561d2f779e9070f4b099f
Clinton <clinton_ferraz@hotmail.com>
2020-04-16 22:15:44 -0300

    fixing more bugs and adding the year to the footer

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 showMenu = () => {
-    dropMenu = document.querySelector(".drop-menu");
-    dropMenu.classList.remove("hidden");
+    dropMenu = document.querySelector('.drop-menu');
+    dropMenu.classList.remove('hidden');
 };
 
 hideMenu = () => {
-    dropMenu = document.querySelector(".drop-menu");
-    dropMenu.classList.add("hidden");
+    dropMenu = document.querySelector('.drop-menu');
+    dropMenu.classList.add('hidden');
 };
+
+document.querySelector('.footer').innerHTML = 'Copyright © GSC - ' + new Date().getFullYear();
